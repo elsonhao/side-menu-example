0ca8be27d7031ea74cf4e433964b67467ae53475
elson Huang <elson.huang@jamzoo.com.tw>
2017-05-25 15:07:15 +0800

    側邊選單範例 完成版

--- slide menu test/ViewController.swift
+++ slide menu test/ViewController.swift
@@ -10,16 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var menuView: UIView!
+    var showmenu = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        menuView.layer.shadowOpacity = 1
+        menuView.layer.shadowRadius = 6
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func openMenu(_ sender: UIBarButtonItem) {
+        if showmenu{
+            leadingConstraint.constant = -150
+            UIView.animate(withDuration: 0.3, animations: {
+                self.view.layoutIfNeeded()
+                
+            })
+        }else{
+            leadingConstraint.constant = 0
+            UIView.animate(withDuration: 0.3, animations: { 
+                self.view.layoutIfNeeded()
+            
+            })
+            
+        }
+        showmenu = !showmenu
+        
     }
-
+    
+    
+   
 
 }
 
